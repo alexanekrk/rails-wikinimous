9b64ac79e6caedd116c062b1718b2ff3a95f3e16
Alexane Kurek <alexane.kurek7@gmail.com>
2023-05-23 16:58:33 -0400

    controller ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Article.destroy_all
+
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Book.genre
+  )
+  article.save
+end
